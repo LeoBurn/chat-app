bda05c90fae3fc3dbf66fed6e57c8ae178754375
Leandro Viana <LeandroViana1988@gmail.com>
2023-09-30 17:07:42 +0100

    First Version Without Auth and only with one hub using Azure SignalR Service

---
+++ v1/src/SignalRFunctionApp/Functions.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+
+namespace SignalRFunctionApp
+{
+    public static class Functions
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "chat")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("messages")]
+        public static Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] object message,
+            [SignalR(HubName = "chat")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "newMessage",
+                    Arguments = new[] { message }
+                });
+        }
+    }
+}
